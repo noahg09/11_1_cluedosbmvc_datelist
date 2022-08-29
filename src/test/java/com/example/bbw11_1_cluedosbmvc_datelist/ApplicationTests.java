cdb868e2cf0c2e19b0f407a33d0e6c2b0cb50d51
noahg09 <noah.gruenig@hispeed.ch>
2022-08-29 15:58:22 +0200

    Spring Boot MVC project hastobeinitialized.

---
+++ src/test/java/com/example/bbw11_1_cluedosbmvc_datelist/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbw11_1_cluedosbmvc_datelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
